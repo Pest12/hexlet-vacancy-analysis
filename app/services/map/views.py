0c0dc67fca098f6b7bb192caee3421079a882f78
tulolo287 <iv1055@yandex.ru>
2026-05-26 21:13:41 +0300

    feat: add region custom manager

--- app/services/map/views.py
+++ app/services/map/views.py
@@ -1,4 +1,3 @@
-from django.db.models import Count, F
 from django.views import View
 from inertia import render as inertia_render
 
@@ -7,11 +6,6 @@ from app.services.vacancies.models import Vacancy
 
 class MapListView(View):
     def get(self, request):
-        map_data = list(
-            Vacancy.objects.filter(region__isnull=False)
-            .values("region")
-            .annotate(totalVacancies=Count("region"))
-            .order_by("region")
-        )
+        map_data = list(Vacancy.regions.total_vacancies())
 
         return inertia_render(request, "MapPage", props={"mapData": map_data})
